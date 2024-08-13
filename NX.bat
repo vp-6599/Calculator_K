0607bbe59e7425dce8f3b28fc5c7fee9edb27125
vp-6599 <vaibhavpachore25@gmail.com>
2024-08-13 19:44:22 +0530

    Add files via upload

---
+++ NX.bat
@@ -0,0 +1,13 @@
+@ECHO ON 
+echo. 
+set UGII_BASE_DIR=C:\progra~1\Siemens\NX2212\UGII
+set JAVA_HOME=C:\progra~1\Zulu\zulu-11-jre
+set FMS_HOME=D:\TC_KNUAF\tccs\tccs
+:: ---TCCS setup--- 
+set UGII_TCCS_ENV_NAME=uat 
+:: 
+:: Run Teamcenter Integration with NX 
+:: with Teamcenter as a background process 
+:: 
+"C:\progra~1\Siemens\NX2212\UGII\ugraf.exe" -pim=yes 
+: THE_END
